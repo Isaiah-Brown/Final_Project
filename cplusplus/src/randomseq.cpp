e5ec7b2c9117a25d1239f0ffc3f2b33fcda1554e
elee2 <elee2@cs-122>
2022-12-06 16:51:51 -0500

    random protein generator

---
+++ cplusplus/src/randomseq.cpp
@@ -0,0 +1,6 @@
+#include <random>
+
+using namespace std;
+int main(){
+    vector<char> aminoacids = {'A','R','N','D','C','Q','E','G','H', 'I', 'L','K', 'M', 'F', 'P','S' , 'T' ,'W' ,'Y' ,'V' , 'B' ,'J' , 'Z' ,'X' ,};
+}
